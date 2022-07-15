94b1516603d6a0737ec5019aa29dd697825e08c3
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-15 15:23:06 +0100

    ruteo

---
+++ routes/novedades.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('novedades', {
+        isNovedades: true
+    }); /* view/novedades.hbs */
+    
+})
+
+
+
+module.exports = router;
