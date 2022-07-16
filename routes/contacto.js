47549c1f8215d2f6d4a328771f9b61b28ccaac17
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-16 11:16:50 +0100

    resolvimos el envio de formulario a un mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/', function(req, res, next) {
     res.render('contacto', {
@@ -7,6 +8,37 @@ router.get('/', function(req, res, next) {
     }); /* view/contactpo.hbs */
     
 })
+
+router.post('/', async function (req, res, next) {
+   /* captura de datos  */
+/*     console.log(req.body) */
+var nombre = req.body.nombre;
+var mail = req.body.mail;
+var telefono = req.body.telefono;
+var comentario = req.body.comentario;
+
+/* console.log(req.body.comentario)  */
+var obj = {
+    to: 'ornella@hotmail.com',
+    subject: 'contacto desde la pagina web',
+    html: nombre + 'se contacto desde la pagina web y quiere saber mas info a este correo' + mail + '<br> Su tel es: ' + telefono + '. Su comentario es: ' + comentario + '.'
+
+}
+var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+}) /* finaliza el transport */
+var info = await transport.sendMail(obj);
+res.render('contacto', {
+    message: 'Mensaje enviado correctamente'
+})
+})
+
+/* finaliza router.post */
 
 
 
