4da221436fe747239241578b0efd830475fee075
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-19 00:17:13 +0100

    diseño y query de agregar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -7,4 +7,15 @@ async function getNovedades() {
     
 }
 
-module.exports = {getNovedades}
+async function insertNovedades(obj){
+    try{
+var query = 'insert into novedades set ?'
+var rows = await pool.query(query, [obj]);
+return rows
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = {getNovedades, insertNovedades}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -16,5 +16,11 @@ router.get('/', async function(req, res, next) {
     }); /* view/admin/novedades.hbs */
     
 })/* cierro get */
-
+/* las flechas vienen a reemplazar la palabra funcion */
+/* esto sirve para mostrar el form de alta las novedades */
+router.get('/agregar', (req, res, next) => {
+res.render('admin/agregar', {
+    layout:'admin/layout'
+})
+})
 module.exports=router;
