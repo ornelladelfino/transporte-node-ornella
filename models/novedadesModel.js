ad437f5b8d90eff794bacf0bfa1338514c949d2d
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-21 00:47:38 +0100

    modificar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -23,11 +23,23 @@ async function deleteNovedadByID(id){
         return rows;
 
     }
-
+/* esta funcion me tare la novedad y el contenido */
 async function getNovedadesByID(id) {
 var query = 'select * from novedades where id=?';
 var rows = await pool.query(query, [id]);
-return rows;    
+return rows[0];    
 }
 
-module.exports = {getNovedades, insertNovedades, deleteNovedadByID, getNovedadesByID}
+async function modificarNovedadByID(obj, id){
+    try {
+        var query = 'update novedades set ? where id=?'
+        var rows = await pool.query(query, [obj, id]);
+        return rows
+        
+    } catch (error) {
+        throw error;
+        /* console.log(error) */
+    }
+}
+
+module.exports = {getNovedades, insertNovedades, deleteNovedadByID, getNovedadesByID, modificarNovedadByID}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -64,9 +64,26 @@ router.get('/modificar/:id', async(req, res, next) =>{
     res.render('admin/modificar', {
         layout: 'admin/layout',
         novedad
-   
      
 })
+})
+router.post('/modificar', async(req, res, next) =>{
+    try{
+        var obj = {
+            titulo:req.body.titulo,
+            subtitulo:req.body.subtitulo, 
+            cuerpo:req.body.cuerpo
+        }
+        await novedadesModel.modificarNovedadByID(obj, req.body.id)
+        res.redirect('/admin/novedades')
+    }catch(error){
+console.log(error)
+res.render('admin/modificar', {
+    layout: 'admin/layout',
+    error: true,
+    message: 'No se modifico la novedad '
+}) 
+    }
 })
 
 module.exports=router;
