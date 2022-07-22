647ff04dba57ece95ee90d7cd8837b9178541efb
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-22 19:32:24 +0100

    buscador front

--- routes/novedades.js
+++ routes/novedades.js
@@ -3,10 +3,18 @@ var router = express.Router();
 var novedadesModel = require('../models/novedadesModel')
 
 router.get('/', async function(req, res, next) {
-    var novedades = await novedadesModel.getNovedades();
+   /*  var novedades = await novedadesModel.getNovedades(); */
+   var novedades;
+   if(req.query.q === undefined){
+       novedades = await novedadesModel.getNovedades();
+   }else{
+       novedades = await novedadesModel.buscarNovedades(req.query.q);
+   }
     res.render('novedades', {
         isNovedades: true,
-        novedades /* tengo los registros para poder imprimirlos en  el hbs */
+        novedades,
+        is_search: req.query.q !== undefined,
+        q:req.query.q /* tengo los registros para poder imprimirlos en  el hbs */
     }); /* view/novedades.hbs */
     
 })
